8d2bd346371cd4dbcd5cb8255e554b19b5a3830b
Mariano Melchiorre <marianomelk@gmail.com>
2025-05-21 11:28:53 -0300

    se agrega comentarios en el archivo main.py

--- main.py
+++ main.py
@@ -1,4 +1,54 @@
-
+# ==============================================================================
+# Propósito del Sistema:
+#
+# Este programa es una aplicacion por consola funcionamiento básico de un sistema 
+# de gestión de pasajes de una empresa SkyRoute. El objetivo es permitir
+# gestionar clientes, destinos y ventas de pasajes. El sistema premite registrar
+#  clientes y destinos, ventas, visualizarlas, modificarlas y eliminarlas, mediante el botón de
+# arrepentimiento. El sistema también permite ademas consultar ventas y generar reportes, 
+# mediante una funcionalidad de "botón de arrepentimiento".
+# El proyecto final utilizará una base de datos relacional para la persistencia
+# de los datos.
+# 
+# Cómo instalar y ejecutar el programa:
+#
+# Para instalar y ejecutar este programa, siga los siguientes pasos:
+#
+# 1.  Instalar Python:
+#     Este programa requiere tener instalado el intérprete de Python (se
+#     recomienda la versión más reciente compatible con su sistema operativo).
+#     a. Descargue el instalador desde el sitio oficial: Python.org
+#     b. Ejecute el instalador. Es **importante** seleccionar la opción para
+#        "Agregar Python al PATH del sistema" durante la instalación.
+#     c. Verifique la instalación abriendo una terminal o línea de comandos
+#        y ejecutando el comando: `py --version` (o `python --version`
+#        o `python3 --version`, dependiendo de su sistema operativo).
+#        Debería ver la versión de Python instalada.
+#
+# 2.  Obtener el archivo del programa:
+#     Obtenga el archivo `main.py` que contiene el código fuente del prototipo.
+#     Esto puede ser descargando el archivo directamente o clonando el repositorio
+#     de https://github.com/nanomelk/abp_programador.git.
+#
+# 3.  Ejecutar el programa:
+#     a. Abra una terminal o línea de comandos.
+#     b. Navegue hasta la carpeta donde se encuentra el archivo `main.py`.
+#        Puede usar el comando `cd` (Change Directory).
+#     c. Ejecute el script de Python utilizando el comando del intérprete:
+#        `python main.py` (o `python3 main.py` si es el comando para su instalación) [17].
+#     d. El programa se iniciará y debería mostrar un menú de opciones
+#        en la consola [11]. Interactúe con el programa seleccionando las
+#        opciones del menú.
+#
+#   Datos de los integrantes del grupo:
+#
+# - Mechiorre Mariano Sebastián, DNI: 29.252.427
+# - Melchorre Alejandra Cristina, DNI: 24.286.870
+# - Roque Martín Miguel, DNI: 23.824.997
+# - Quispe Christian, DNI 23.198.068
+# - Heredia Joel, DNI 41.158.023
+#
+# ==============================================================================
 def main_menu():
     while True:
         """
@@ -44,7 +94,6 @@ def main_menu():
                 if menuClientes == 1:
                         print("Lista de Clientes")
                         input("presiona enter para salir...")
-                        break
                 elif menuClientes == 2:
                         print("Ingrese los datos del Nuevo Cliente")
                         nombre = input("Nombre: ")
@@ -53,9 +102,8 @@ def main_menu():
                         telefono = input("Telefono: ")
                         email = input("Email: ")
                         print("Cliente registrado con exito")
-                        print("Nombre:", nombre, "Apellido:", apellido, "fechaNacimiento:", fechaNacimiento, "Telefono:", telefono, "Email:", email)           
+                        print("Se guardó el cliente, sus datos son: Nombre:", nombre, "Apellido:", apellido, "fechaNacimiento:", fechaNacimiento, "Telefono:", telefono, "Email:", email)           
                         input("presiona enter para salir...")
-                        break
                 elif menuClientes == 3:
                         print("Modificar Cliente")
                         print("Ingrese el ID del cliente a modificar")
@@ -65,11 +113,9 @@ def main_menu():
                         print("Ingrese el ID del cliente a borrar")
                         input("presiona enter para salir...")    
                 elif menuClientes == 5:
-                        input("presiona enter para salir...")
                         break
                 else: 
                         print("Opcion Invalida-ingrese una opcion valida")
-                        break
         elif menu == 2:
             print("You selected Option 2")
             input("Press Enter to continue...")
@@ -77,8 +123,7 @@ def main_menu():
             print("Exiting...")
             break
         else:
-            print("Invalid choice. Please try again.")
-            input("Press Enter to continue...")
-
+            print("Opcion Invalida-ingrese una opcion valida")
+            input("presiona enter para volver al menu anterior...") 
 if __name__ == "__main__":
     main_menu()
